1804ebe79a8a8a5b5752049f56d64facc253d9a0
Nils Schneider <nils@nilsschneider.net>
2016-04-14 19:28:16 +0200

    es hat sich mal wieder was geändert

--- klingel.py
+++ klingel.py
@@ -6,12 +6,14 @@ import random
 import socket
 import struct
 import time
-from threading import Timer
-from subprocess import call
+import requests
+import json
+from threading import Timer, Thread
+from subprocess import call, Popen
 
 def playsound(fn):
 	print("play", fn)
-	call(["paplay", fn])
+	Popen(["paplay", fn])
 	return
 	CHUNK = 1024
 
@@ -54,34 +56,69 @@ ifname = "eth0"
 
 s = receiver(group, port)
 
+locked_oben = True
+locked_unten = True
+
+def reset_locks():
+	global locked_open, locked_unten
+	locked_oben = True
+	locked_unten = True
+
 def genugdavon():
 	putfile('/sys/class/gpio/gpio11/value', '1')
 	playsound("/root/wrong.wav")
 
+def check_locks():
+	try:
+		r = requests.get('https://padlock.nobreakspace.org/api/locks', cert=('klingel.crt', 'klingel.key'), verify=False)
+		for x in r.json():
+			if x['id'] == '261175':
+				locked_unten = x['locked']
+
+			if x['id'] == '334EC1':
+				locked_oben = x['locked']
+
+		print("Locks", locked_unten, locked_oben)
+	except:
+		pass
+
 timer = None
 
-def run(data):
+def run(data, d=None):
 	global timer
 	print(data)
 
 	if data == "ring":
-		if timer:
-			timer.cancel()
+		if int(d) == 1:
+			if timer:
+				timer.cancel()
 
-		timer = Timer(60, genugdavon)
-		timer.start()
-		putfile('/sys/class/gpio/gpio11/value', '0')
-		playsound("/root/ring.wav")
+			timer = Timer(60, genugdavon)
+			timer.start()
+
+			check = Thread(target=check_locks)
+			check.start()
+
+			putfile('/sys/class/gpio/gpio11/value', '0')
+
+			playsound("/root/ring-bb.wav")
+		else:
+			playsound("/root/ring-fis.wav")
+	
 
 	if data == "open":
+#		if not locked_oben:
 		playsound("/root/open.wav")
 	
 	if data == "summ":
 		if timer:
 			timer.cancel()
 
+#		if not locked_oben:
 		putfile('/sys/class/gpio/gpio11/value', '1')
 		playsound("/root/summ.wav")
+#		else:
+#			playsound("/root/locked.wav")
 
 def putfile(file, data):
 	f = open(file, 'w')
@@ -101,7 +138,21 @@ while True:
 	data, sender = s.recvfrom(1500)
 	while data[-1:] == '\0': data = data[:-1] # Strip trailing \0's
 	data = data.decode('utf-8')
-	(data, seq) = data.split(",")
+	x = data.split(",")
+	try:
+		data = x[0]
+	except:
+		data = None
+
+	try:
+		seq = x[1]
+	except:
+		seq = None
+
+	try:
+		d = x[2]
+	except:
+		d = None
 
 	try:
 	  l = lastseq[data]
@@ -109,6 +160,6 @@ while True:
 	  l = 0
 	
 	if seq != l:
-		run(data)
+		run(data, d)
 		lastseq[data] = seq
 
